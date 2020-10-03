8796cbe5698503535cffcfc040556346eddb1b61
Riya Gupta <Riyaktl2000@gmail.com>
2020-10-03 20:32:21 +0530

    add

---
+++ C++/fizz_buzz_riya.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+ 
+{
+    int i;
+    for(i=1; i<=100; i++)
+    {
+        if(i%3==0)
+        {
+            cout<<"Fizz ";
+        }
+       else if(i%5==0)
+       {
+            cout<<"buzz ";
+       }
+       else
+       {
+           cout<<i<<" ";
+       }
+}}
